12fbc72d6cfda74c06dd159c38a3d25403912c0f
ppruvost <patrick.pruvost50@gmail.com>
2026-01-12 22:13:15 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import Scene from './components/Scene'
+
 export default function App() {
   return (
-    <div
-      style={{
-        color: 'white',
-        padding: '40px',
-        fontSize: '24px'
-      }}
-    >
-      ✅ React fonctionne sur GitHub Pages
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas camera={{ position: [0, 1.6, 3], fov: 50 }}>
+        {/* Lumières */}
+        <ambientLight intensity={0.6} />
+        <directionalLight position={[5, 5, 5]} intensity={1} />
+
+        {/* Scène 3D */}
+        <Scene />
+
+        {/* Contrôles caméra */}
+        <OrbitControls enablePan enableZoom enableRotate />
+      </Canvas>
     </div>
   )
 }
+
 
