4ed1d85984a669132ce0692587caa4386009cbbd
ppruvost <patrick.pruvost50@gmail.com>
2026-01-12 21:52:19 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root')
+
+if (!container) {
+  throw new Error("Root container missing in index.html")
+}
+
+ReactDOM.createRoot(container).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
