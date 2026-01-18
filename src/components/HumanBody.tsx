3effb462e6f9ec1b26139d2940e098b4900b0149
ppruvost <patrick.pruvost50@gmail.com>
2026-01-18 07:33:16 +0100

    Delete src directory

--- src/components/HumanBody.tsx
+++
@@ -1,6 +0,0 @@
-import { useGLTF } from '@react-three/drei'
-
-export default function HumanBody() {
-  const { scene } = useGLTF('/models/human_body.glb')
-  return <primitive object={scene} scale={1.2} />
-}
