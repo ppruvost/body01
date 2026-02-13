4837ada546256ff1ce201f9f0316a2e044de0f65
ppruvost <patrick.pruvost50@gmail.com>
2026-02-13 14:34:41 +0100

    Update precomputeMeridians.js

--- src/js/precomputeMeridians.js
+++ src/js/precomputeMeridians.js
@@ -1,21 +1,16 @@
-function buildMeridianLines(scene) {
+function buildMeridianCurves_r71(scene, curvatureFactor) {
 
     var meridianGroups = {};
 
-    // Regrouper les points par méridien
     ACU_POINTS.forEach(function(p) {
-        if (!meridianGroups[p.meridian]) {
-            meridianGroups[p.meridian] = [];
-        }
+        if (!meridianGroups[p.meridian]) meridianGroups[p.meridian] = [];
         meridianGroups[p.meridian].push(p);
     });
 
-    // Pour chaque méridien
     Object.keys(meridianGroups).forEach(function(meridian) {
 
         var points = meridianGroups[meridian];
 
-        // Tri basé sur le numéro contenu dans le nom
         points.sort(function(a, b) {
             var na = parseInt(a.name.replace(/\D/g, ''));
             var nb = parseInt(b.name.replace(/\D/g, ''));
@@ -24,11 +19,32 @@ function buildMeridianLines(scene) {
 
         var geometry = new THREE.Geometry();
 
-        points.forEach(function(p) {
-            geometry.vertices.push(
-                new THREE.Vector3(p.x, p.y, p.z)
-            );
-        });
+        for (var i = 0; i < points.length - 1; i++) {
+
+            var p1 = new THREE.Vector3(points[i].x, points[i].y, points[i].z);
+            var p2 = new THREE.Vector3(points[i+1].x, points[i+1].y, points[i+1].z);
+
+            // Point médian
+            var mid = new THREE.Vector3().addVectors(p1, p2).multiplyScalar(0.5);
+
+            // Direction perpendiculaire simple pour la courbure dans le plan XY
+            var dir = new THREE.Vector3().subVectors(p2, p1);
+            var perp = new THREE.Vector3(-dir.y, dir.x, 0).normalize().multiplyScalar(curvatureFactor * (p1.z >= 0 ? 1 : -1));
+            mid.add(perp);
+
+            // Diviser en N points pour simuler la courbe
+            var segments = 10;
+            for (var s = 0; s <= segments; s++) {
+                var t = s / segments;
+
+                // Quadratic Bézier formula
+                var x = (1-t)*(1-t)*p1.x + 2*(1-t)*t*mid.x + t*t*p2.x;
+                var y = (1-t)*(1-t)*p1.y + 2*(1-t)*t*mid.y + t*t*p2.y;
+                var z = (1-t)*(1-t)*p1.z + 2*(1-t)*t*mid.z + t*t*p2.z;
+
+                geometry.vertices.push(new THREE.Vector3(x, y, z));
+            }
+        }
 
         var material = new THREE.LineBasicMaterial({
             color: getMeridianColor(meridian),
