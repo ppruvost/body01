49feb002623d4db87de11b869f4bec78ffcaa2d4
ppruvost <patrick.pruvost50@gmail.com>
2026-02-13 10:34:56 +0100

    Update precomputeMeridians.js

--- src/js/precomputeMeridians.js
+++ src/js/precomputeMeridians.js
@@ -1,51 +1,70 @@
-/* ============================================================
-   PRECOMPUTE MERIDIANS LINES
-   ============================================================ */
-
 function buildMeridianLines(scene) {
 
-    const meridianGroups = {};
+    var meridianGroups = {};
 
-    // Regrouper les points par méridien
-    ACU_POINTS.forEach(p => {
-        if (!meridianGroups[p.meridian]) {
+    // Regroupement par méridien
+    ACU_POINTS.forEach(function(p){
+        if(!meridianGroups[p.meridian]){
             meridianGroups[p.meridian] = [];
         }
         meridianGroups[p.meridian].push(p);
     });
 
-    // Pour chaque méridien
-    Object.keys(meridianGroups).forEach(meridian => {
+    Object.keys(meridianGroups).forEach(function(meridian){
 
-        const points = meridianGroups[meridian];
+        var points = meridianGroups[meridian];
 
-        // Tri intelligent basé sur le nom (ex: p1, p2, gi3...)
-        points.sort((a, b) => {
-            const na = parseInt(a.name.replace(/\D/g, ''));
-            const nb = parseInt(b.name.replace(/\D/g, ''));
+        // tri numérique (p1, p2, gi3...)
+        points.sort(function(a,b){
+            var na = parseInt(a.name.replace(/\D/g,''));
+            var nb = parseInt(b.name.replace(/\D/g,''));
             return na - nb;
         });
 
-        const positions = [];
+        var vectors = [];
 
-        points.forEach(p => {
-            positions.push(p.x, p.y, p.z);
-        });
+        for(var i=0; i<points.length; i++){
+            vectors.push(
+                new THREE.Vector3(
+                    points[i].x,
+                    points[i].y,
+                    points[i].z
+                )
+            );
+        }
 
-        const geometry = new THREE.BufferGeometry();
-        geometry.setAttribute(
+        // Courbe CatmullRom
+        var curve = new THREE.CatmullRomCurve3(vectors);
+
+        // échantillonnage
+        var sampled = curve.getPoints(60);
+
+        var geometry = new THREE.BufferGeometry();
+
+        var positions = new Float32Array(sampled.length * 3);
+
+        for(var j=0; j<sampled.length; j++){
+
+            // léger offset pour éviter de passer sous le mesh
+            var v = sampled[j].clone().multiplyScalar(1.01);
+
+            positions[j*3]     = v.x;
+            positions[j*3 + 1] = v.y;
+            positions[j*3 + 2] = v.z;
+        }
+
+        geometry.addAttribute(
             'position',
-            new THREE.Float32BufferAttribute(positions, 3)
+            new THREE.BufferAttribute(positions, 3)
         );
 
-        const material = new THREE.LineBasicMaterial({
+        var material = new THREE.LineBasicMaterial({
             color: getMeridianColor(meridian),
             transparent: true,
             opacity: 0.6
         });
 
-        const line = new THREE.Line(geometry, material);
-        line.name = "line_" + meridian;
+        var line = new THREE.Line(geometry, material);
 
         scene.add(line);
     });
