97b2c9bf76436629efdbd7f159f60689154fca09
ppruvost <patrick.pruvost50@gmail.com>
2026-02-14 10:19:01 +0100

    Update precomputeMeridians.js

--- src/js/precomputeMeridians.js
+++ src/js/precomputeMeridians.js
@@ -26,7 +26,7 @@ function buildMeridianCurves(scene) {
             var p1 = points[i];
             var p2 = points[i + 1];
             var distance = calculateDistance(p1, p2);
-            var curvatureFactor = distance > 5 ? 1.3 : 0.4;
+            var curvatureFactor = distance > 6 ? 1.3 : 0.3;
 
             var geometry = new THREE.Geometry();
             geometry.vertices.push(new THREE.Vector3(p1.x, p1.y, p1.z));
