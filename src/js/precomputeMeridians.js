19001f467a6a01369018e2757cfdf79b4f36558e
ppruvost <patrick.pruvost50@gmail.com>
2026-02-14 09:08:58 +0100

    Update precomputeMeridians.js

--- src/js/precomputeMeridians.js
+++ src/js/precomputeMeridians.js
@@ -25,32 +25,22 @@ function buildMeridianCurves_r71(scene) {
             var p1 = points[i];
             var p2 = points[i + 1];
             var distance = calculateDistance(p1, p2);
-
-            // Ajustement dynamique du curvatureFactor en fonction de la distance
             var curvatureFactor = distance > 5 ? 1.2 : 0.6;
 
-            var geometry = new THREE.BufferGeometry();
-            var vertices = [];
-
-            // Point de départ
-            vertices.push(p1.x, p1.y, p1.z);
+            var geometry = new THREE.Geometry();
+            geometry.vertices.push(new THREE.Vector3(p1.x, p1.y, p1.z));
 
             var segments = 20;
             for (var s = 1; s <= segments; s++) {
                 var t = s / segments;
                 var x = p1.x + (p2.x - p1.x) * t;
                 var y = p1.y + (p2.y - p1.y) * t;
-                var z = p1.z + (p2.z - p1.z) * t;
-
-                // Ajout d'une courbure douce sans artefact
-                var midZ = z + curvatureFactor * Math.sin(t * Math.PI);
-                vertices.push(x, y, midZ);
+                var z = p1.z + (p2.z - p1.z) * t + curvatureFactor * Math.sin(t * Math.PI);
+                geometry.vertices.push(new THREE.Vector3(x, y, z));
             }
 
-            // Point d'arrivée
-            vertices.push(p2.x, p2.y, p2.z);
+            geometry.vertices.push(new THREE.Vector3(p2.x, p2.y, p2.z));
 
-            geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
             var material = new THREE.LineBasicMaterial({
                 color: getMeridianColor(meridian),
                 transparent: true,
