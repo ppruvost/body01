5d1a445b56837daf0fe0a9bbe25bdecc8df5057a
ppruvost <patrick.pruvost50@gmail.com>
2026-02-19 11:18:07 +0100

    Update precomputeMeridians.js

--- src/js/precomputeMeridians.js
+++ src/js/precomputeMeridians.js
@@ -5,8 +5,8 @@ var sphereRadius = 0.3;
 // Définition des cas particuliers de courbes avec paramètres individuels
 var SPECIAL_CURVES = {
     // Courbes méridien poumon
-    "p2-p3": "(1.75;2.05;0.8|1.5|1|0.8|1.55)",  // (z25;z50;z75|angle|ventreDos|peakFactor|parabolaFactor)
-    "p2-r-p3-r": "(1.75;2.05;0.8|-1.5|1|0.8|1.55)",
+    "p2-p3": "(1.75;2.05;0.8|1.5|1|0.6|1.55)",  // (z25;z50;z75|angle|ventreDos|peakFactor|parabolaFactor)
+    "p2-r-p3-r": "(1.75;2.05;0.8|-1.5|1|0.6|1.55)",
 
     // Courbes méridien estomac
     "e5-e6": "(;;|4|1|0.6|1.6)",  // (z25;z50;z75|angle|ventreDos|peakFactor|parabolaFactor)
@@ -15,8 +15,8 @@ var SPECIAL_CURVES = {
     "e16-e17": "(0.05;0.25;0.05|4|1|0.6|1.6)",
     "e16-r-e17-r": "(0.05;0.25;0.05|-4|1|0.6|1.6)",
 
-    "e11-e12": "(;0.5;|5|1|1.1|1.4)",
-    "e11-r-e12-r": "(;0.5;|-5|1|1.1|1.4)",
+    "e11-e12": "(;0.5;|5|1|0.5|1.4)",
+    "e11-r-e12-r": "(;0.5;|-5|1|0.5|1.4)",
 
     "e17-e18": "(0.3;0.8;0.4|4|1|0.6|1.5)",
     "e17-r-e18-r": "(0.08;0.35;0.1|-4|1|0.6|1.5)",
