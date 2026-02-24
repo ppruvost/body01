9e559ce6a477cb7e1e1443a9690724822f07ede4
ppruvost <patrick.pruvost50@gmail.com>
2026-02-24 12:09:32 +0100

    Update acu_point.js

--- src/js/acu_point.js
+++ src/js/acu_point.js
@@ -215,7 +215,7 @@ const ACU_POINTS = [
     { name: "v8-r", x: -18, y: -6, z: -0.85, meridian: "Vessie-r" },
     { name: "v9-r", x: -17, y: -4, z: -2, meridian: "Vessie-r" },
 
-	{ name: "v1", x: 0.93, y: 67, z: 3, meridian: "Vessie" },
+	{ name: "v1", x: 0.93, y: 67, z: 3.3, meridian: "Vessie" },
     { name: "v2", x: 24, y: -18, z: 5, meridian: "Vessie" },
     { name: "V3", x: 23, y: -16, z: 4, meridian: "Vessie" },
     { name: "v4", x: 22, y: -14, z: 3, meridian: "Vessie" },
